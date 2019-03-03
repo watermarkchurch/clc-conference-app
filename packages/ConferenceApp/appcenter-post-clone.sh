ad5a5b9b152af240da8c7ddd0eda32cfd457fc0d
Conrad VanLandingham <hi@convan.me>
2019-03-02 23:27:13 -0600

    downgrading node to newest version appcenter supports

--- packages/ConferenceApp/appcenter-post-clone.sh
+++ packages/ConferenceApp/appcenter-post-clone.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -ex
 brew uninstall node@6
-NODE_VERSION="11.6.0"
+NODE_VERSION="8.15.0"
 curl "https://nodejs.org/dist/v${NODE_VERSION}/node-v${NODE_VERSION}.pkg" > "$HOME/Downloads/node-installer.pkg"
 sudo installer -store -pkg "$HOME/Downloads/node-installer.pkg" -target "/"
 
