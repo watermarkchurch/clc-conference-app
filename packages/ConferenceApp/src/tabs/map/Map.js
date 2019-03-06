0b5387be675fc2434ed3de70f89307618d5cd1c3
Conrad VanLandingham <conrad@differential.com>
2019-03-06 08:28:26 -0600

    add map to map tab (#7)
    * add map to map tab
    
    * remove inline style

--- packages/ConferenceApp/src/tabs/map/Map.js
+++ packages/ConferenceApp/src/tabs/map/Map.js
@@ -1,7 +1,22 @@
 import React, { PureComponent } from 'react';
-import { BackgroundView } from '@apollosproject/ui-kit';
+import { styled } from '@apollosproject/ui-kit';
+import { ScrollView, Image, StyleSheet } from 'react-native';
 import headerOptions from '../headerOptions';
 
+const styles = StyleSheet.create({
+  contentContainerStyle: { width: '100%', height: '100%' },
+});
+
+const SizedImage = styled({
+  resizeMode: 'cover',
+  width: '100%',
+  height: '100%',
+})(Image);
+
+const ImageZoomView = styled({
+  width: '100%',
+  height: '100%',
+})(ScrollView);
 class Map extends PureComponent {
   static navigationOptions = {
     title: 'Map',
@@ -9,7 +24,20 @@ class Map extends PureComponent {
   };
 
   render() {
-    return <BackgroundView />;
+    return (
+      <ImageZoomView
+        horizontal
+        directionalLockEnabled={false}
+        scrollEventThrottle={100}
+        showsHorizontalScrollIndicator={false}
+        showsVerticalScrollIndicator={false}
+        maximumZoomScale={1.0001}
+        contentContainerStyle={styles.contentContainerStyle}
+        bouncesZoom
+      >
+        <SizedImage source={require('./map.jpg')} />
+      </ImageZoomView>
+    );
   }
 }
 
